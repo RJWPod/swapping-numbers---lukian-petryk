17d3c24563a2cac5907c8f77e78d2c606ee98433
RJWPod <101339801+RJWPod@users.noreply.github.com>
2022-03-24 08:53:32 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,45 @@
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showString("x")
+    Number_1 += -1
+    basic.showNumber(Number_1)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showString("x")
+    Number_1 += 1
+    basic.showNumber(Number_1)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P2, function () {
+    basic.showString("y")
+    Number_2 += -1
+    basic.showNumber(Number_2)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("y")
+    basic.showNumber(Number_2)
+    basic.showString("x")
+    basic.showNumber(Number_1)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("y")
+    Number_2 += 1
+    basic.showNumber(Number_2)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showString("x")
+    Number_1 = 0
+    basic.showNumber(Number_1)
+    basic.showString("y")
+    Number_2 = 0
+    basic.showNumber(Number_2)
+    basic.clearScreen()
+})
+let Number_2 = 0
+let Number_1 = 0
+basic.showString("x=#1")
+basic.showString("y=#2")
+basic.showIcon(IconNames.No)
